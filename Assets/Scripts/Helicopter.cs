d640d2dd2954b8b5122a658073d5928ead232d7a
Wojciech Stachowiak <stachowiakw@gmail.com>
2018-11-29 09:13:34 +0100

    L279

--- Assets/Scripts/Helicopter.cs
+++ Assets/Scripts/Helicopter.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 
 public class Helicopter : MonoBehaviour {
     bool HeliCalled;
+    public TerrainScaner terrainScaner;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         HeliCalled = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetButton("HeliCall")) { if (HeliCalled == false) { HeliInbound(); } }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("HeliCall"))
+        {
+            if (HeliCalled == false && terrainScaner.IsSpaceToLand() == true)
+            { HeliInbound(); }
+            else
+            { HeliInboundFailed(); }
+        }
     }
 
     void HeliInbound()
-    {
-        HeliCalled = true;
-        print("Helicopter called");
-    }
+        {
+            HeliCalled = true;
+            print("Helicopter called");
+        }
+
+    void HeliInboundFailed()
+        {
+            print("Helicopter calling FAILED");
+        }
+    
 }
---
+++ Assets/TerrainScaner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainScaner : MonoBehaviour
+{
+    public float timer;
+    // Use this for initialization
+    void Start()
+    {
+        timer = 0;
+    }
+
+    private void Update()
+    {
+        timer = timer + Time.deltaTime;
+    }
+
+    private void OnTriggerStay()
+    {
+        timer = 0;
+        print("dotykamCzegos!");
+    }
+
+    public bool IsSpaceToLand()
+    {
+        if (timer>1) { return true; }
+        else { return false; }
+    }
+}
